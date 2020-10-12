2defb018c534e11d429111427601c5008456fa22
jp <jptick@gmail.com>
2020-10-12 16:57:25 -0300

    Create orphanages page

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import React from "react";
+import React from 'react';
 
-import "./styles/global.css";
+import './styles/global.css';
 
-import Routes from "./routes";
+import Routes from './routes';
 
 function App() {
   return <Routes />;
