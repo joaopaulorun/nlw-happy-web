11369b01bedc34538d554eefc8142dc32a0a7740
Joao Paulo <jptick@gmail.com>
2020-10-17 14:48:04 -0300

    Updates

---
+++ precache-manifest.d3f00d741ecd646bca6a181123906046.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a0646938098f7b959c557b67feb34c5",
+    "url": "/nlw-happy-web/index.html"
+  },
+  {
+    "revision": "764854d92ae1edd93f3f",
+    "url": "/nlw-happy-web/static/css/2.a4567992.chunk.css"
+  },
+  {
+    "revision": "eda9a4baeec66ea5482f",
+    "url": "/nlw-happy-web/static/css/main.50f03db7.chunk.css"
+  },
+  {
+    "revision": "764854d92ae1edd93f3f",
+    "url": "/nlw-happy-web/static/js/2.9e6589a5.chunk.js"
+  },
+  {
+    "revision": "3c56d32f1a0e992453f57615992b86b8",
+    "url": "/nlw-happy-web/static/js/2.9e6589a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eda9a4baeec66ea5482f",
+    "url": "/nlw-happy-web/static/js/main.5fb5405d.chunk.js"
+  },
+  {
+    "revision": "1a3ba6a0731d7b535495",
+    "url": "/nlw-happy-web/static/js/runtime-main.2834305b.js"
+  },
+  {
+    "revision": "91c8d1af04a8fbdde51d2b16c5471b15",
+    "url": "/nlw-happy-web/static/media/landing.91c8d1af.svg"
+  },
+  {
+    "revision": "1653c64706c95e2e85a07cc755abf8ba",
+    "url": "/nlw-happy-web/static/media/logo.1653c647.svg"
+  },
+  {
+    "revision": "658b83029253fe8e316b8f364db7b424",
+    "url": "/nlw-happy-web/static/media/map-marker.658b8302.svg"
+  }
+]);
